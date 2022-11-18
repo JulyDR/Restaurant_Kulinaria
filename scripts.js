80b484168e9471a646cd3ce76a412a4841eef2bc
Julia <juliac.kraus@yahoo.de>
2022-11-17 21:07:44 -0400

    Filterable Menu

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@ $( document ).ready(function() {
   });
   
 
-  // Slick Caroussel
-  $('.slider-container').slick({
+// Slick Caroussel
+$('.slider-container').slick({
     autoplay: true, // Do we want it to autoplay? true or false
     autoplaySpeed: 4000, // How long between each slide when auto-playing
     speed: 500, // How fast is the transition in milliseconds
@@ -19,4 +19,36 @@ $( document ).ready(function() {
     infinite: true, // When true, means that it will scroll in a circle
     pauseOnHover: true, // When true means the autoplay pauses when hovering
     pauseOnDotsHover: false, // Pauses the autoplay when hovering over the dots
+  });
+
+// Filterable Menu
+  $('#menu-filter span').click(function(){
+    
+    // Remove class 'active' from any <span> that is currently active 
+    $('#menu-filter .active').removeClass('active');
+      
+    // give this <span> that was clicked on a class of 'active' 
+    $(this).addClass('active');
+  
+    // get the name of the category from this <span>, remove up to two spaces from the text and replace them with dashes, and make it lowercase 
+    var filterVal = $(this).text().replace(' ','-').replace(' ','-').toLowerCase();
+  
+    // This is something new, it's an 'each' function which basically iterates through each element that matches the selector and applies the function one by one.
+    $('#filterable-menu .menu-item').each(function() {
+      
+      // If the filter value that they have clicked on is 'all' then remove the class of hidden from each gallery-item. 
+      if (filterVal == 'all') {
+        $(this).removeClass('hidden');
+      }
+      
+      // If it's not all, then 
+      else {
+        if($(this).hasClass(filterVal)) {
+          $(this).removeClass('hidden'); // show those that have the filter class
+        }
+        else {
+          $(this).addClass('hidden'); // hide those that do not have the filter
+        }
+      }
+    });
   });
